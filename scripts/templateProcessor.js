47ede1da4c8d3203544b4a6129ae07ead4acf756
BeataStultica <54076963+BeataStultica@users.noreply.github.com>
2020-11-25 08:33:37 +0200

    Update templateProcessor.js

--- scripts/templateProcessor.js
+++ scripts/templateProcessor.js
@@ -2,6 +2,7 @@ class TemplateProcessor {
     render(view) {
         const rootNode = document.getElementById('main');
         rootNode.innerHTML = view;
+        window.scrollTo(0,0);
     }
 }
 
